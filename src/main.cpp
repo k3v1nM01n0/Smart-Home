54ee81aaa23b6679a5208dc764ab35ba5f5d66d8
kevindson <kevinmoino10@gmail.com>
2020-12-29 14:10:57 +0300

    init project structure

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include <WiFiClient.h>
+
+#define SSID =
+#define PASS =
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin() = 115200;
+
+  //Connecting to wifi
+  Serial.println();
+  Wifi.mode
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
